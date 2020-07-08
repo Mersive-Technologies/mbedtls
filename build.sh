0d4027bf4b8ecfdebf3e5f7c7fefa61bd2558688
Brent Gardner <bgardner@squarelabs.net>
2020-07-08 06:19:14 -0600

    trigger build
    Need python
    
    Try to cross compile for android
    
    chmod
    
    Use android cmake
    
    Pull in password
    
    Put make on the path
    
    Fix build command
    
    Upgrade cmake
    
    wtf yolo
    
    Add linker
    
    try setting lib dir
    
    throw spaghetti
    
    fix abi?
    
    deploy?
    
    Fix some stuff
    
    Fix some stuff
    
    Try to parameterize, but fail
    
    Try to fix drone build
    
    Output the commands during setup
    
    Output the commands during build
    
    Save environment between drone steps
    
    Specify correct cmake args

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -x
+
+echo "Restoring environment"
+source ~/env.sh
+
+echo "Performing build"
+mkdir -p build
+cd build
+echo "PATH ${PATH}"
+cmake -GNinja -DANDROID_NDK=${ANDROID_NDK_HOME} -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_TOOLCHAIN_FILE=${ANDROID_NDK_HOME}/build/cmake/android.toolchain.cmake \
+  -DANDROID_ABI=${ANDROID_ABI} \
+  -DANDROID_NATIVE_API_LEVEL=${ANDROID_PLATFORM} \
+  ..
+cd -
+cmake --build ./build -- -j`nproc`
---
+++ setup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -x
+
+echo "Some useful env info"
+printenv
+echo ${NEXUS_PWD} | sha256sum
+
+echo "setup env"
+export ANDROID_NDK_HOME=${ANDROID_HOME}/ndk/${NDK_VERSION}
+export TOOLCHAIN=${ANDROID_NDK_HOME}/toolchains/llvm/prebuilt/linux-x86_64
+export PATH=${TOOLCHAIN}/bin:${ANDROID_HOME}/cmake/${CMAKE_VERSION}/bin:${ANDROID_HOME}/tools:${ANDROID_HOME}/tools/bin:${ANDROID_HOME}/platform-tools:$PATH
+
+echo "Installing the necessary packages"
+apt-get -qq update && apt-get -qq install -y openjdk-8-jdk unzip wget curl git python python3
+mkdir -p ~/.ssh/
+mkdir -p /opt/
+
+echo "Downloading and installing the Android SDK"
+export DL_TARGET_SDK=/opt/android-sdk.zip
+curl -sSf --user "vhtest:$NEXUS_PWD" -o $DL_TARGET_SDK https://nexus.mersive.xyz/repository/vhserver/sdk-tools-linux-4333796.zip
+mkdir -p "$ANDROID_HOME"
+unzip -q -d "$ANDROID_HOME" $DL_TARGET_SDK
+
+echo "Configuring the SDK"
+mkdir -p /root/.android
+touch /root/.android/repositories.cfg
+yes | sdkmanager --licenses > /dev/null
+sdkmanager "tools" "platform-tools" "build-tools;${BUILD_TOOLS_VERSION}" "platforms;android-25" > /dev/null
+sdkmanager "cmake;${CMAKE_VERSION}" > /dev/null
+sdkmanager "ndk;${NDK_VERSION}" > /dev/null
+
+echo "Saving environment"
+declare -px > ~/env.sh
